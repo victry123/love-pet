6cd72c4ff56404f1fc36d4715488335f2c7b501b
guo <gfs1528@126.com>
2022-01-17 15:28:58 +0800

    配置了布局适配

--- love-pet-front/src/App.vue
+++ love-pet-front/src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="wrapper">
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box3"></div>
+    <div class="box4"></div>
+    <div class="box5"></div>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style>
+	* {
+	  margin: 0;
+	  padding: 0;
+	}
 </style>
+
+<style scoped>
+	.wrapper div {
+	  height: 1rem;
+	}
+	.box1 {
+	  width: 2rem;
+	  background-color: coral;
+	}
+	.box2 {
+	  width: 4rem;
+	  background-color: skyblue;
+	}
+	.box3 {
+	  width: 6rem;
+	  background-color: palegreen;
+	}
+	.box4 {
+	  width: 8rem;
+	  background-color: wheat;
+	}
+	.box5 {
+	  width: 10rem;
+	  background-color: darkred;
+	}
+</style>
+
