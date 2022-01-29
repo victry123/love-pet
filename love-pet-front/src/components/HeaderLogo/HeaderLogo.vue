5781ede5d7abdf799186058f7fcc29938484d1cb
vic <1248849985@qq.com>
2022-01-29 16:25:03 +0800

    搜索页

---
+++ love-pet-front/src/components/HeaderLogo/HeaderLogo.vue
@@ -0,0 +1,183 @@
+<template>
+  <div id="header">
+    <div id="seach">
+      <div class="logo" :style="{ backgroundImage: `url(${imgurl})` }"></div>
+      <div class="seach">
+        <i class="iconfont icon-sousuoxiao"></i>
+        <form action="" target="_blank">
+          <input
+            type="text"
+            class="inseach"
+            maxlength="100"
+            placeholder="搜索你感兴趣的内容"
+          />
+        </form>
+        <button
+          type="button"
+          class="buseach"
+          @mouseover="smover"
+          @mouseleave="smleave"
+        >
+          <span :class="{ seachmove: seachmover == true }">搜索</span>
+        </button>
+      </div>
+    </div>
+
+    <div class="user">
+      <div v-if="login">
+        <button class="login">登录</button>
+      </div>
+      <div v-else class="user">
+        <button class="login-f">发布</button>
+        <a href=""><img :src="userimgurl" alt="" class="userimg" /></a>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgurl: require("../../assets/img/logo.png"),
+      seachmover: false,
+      login: true,
+      userimgurl: require("../../assets/img/userimg.png"),
+    };
+  },
+  methods: {
+    // 搜索按钮的移入高亮
+    smover() {
+      this.seachmover = true;
+    },
+    smleave() {
+      this.seachmover = false;
+    },
+  },
+};
+</script>
+
+<style>
+.logo {
+  width: 130px;
+  height: 66px;
+  background-repeat: no-repeat;
+  margin: 0;
+  display: flex;
+  flex: 0.5;
+}
+
+#seach{
+  width: 1000px;
+  display: flex;
+  flex-direction: row;
+}
+
+#header {
+  display: flex;
+  width: 100%;
+  height: 60px;
+  background-color: #161722;
+  border-bottom: 1px solid rgba(90, 90, 90, 0.4);
+  align-items: center;
+  box-sizing: border-box;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 32px 0 22px;
+  position: relative;
+  flex-grow: 1;
+  flex-shrink: 0;
+}
+.seach {
+  width: 620px;
+  height: 40px;
+  margin-top: 13px;
+  background-color: #292b35;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  flex-direction: row;
+}
+
+.seach i {
+  font-size: 18px;
+  margin-left: 10px;
+  color: #d4d5d7;
+}
+
+.seach form {
+  flex-grow: 1;
+}
+.inseach {
+  width: 100%;
+  background-color: #292b35;
+  border: none;
+  caret-color: #fe2c55;
+  color: #2f3035;
+  color: aliceblue;
+  flex-grow: 1;
+  font-size: 14px;
+  font-weight: bold;
+  height: 100%;
+  outline: none;
+  padding: 0 6px;
+}
+
+.seach button {
+  width: 64px;
+  height: 100%;
+  background-color: #292b35;
+  align-items: center;
+  color: #eff0f3;
+  border: none;
+  border-bottom-right-radius: 4px;
+  border-top-right-radius: 4px;
+  font-weight: bolder;
+  font-size: 16px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+}
+
+.user {
+  width: 300px;
+  height: 40px;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+
+.login,
+.login-f {
+  width: 80px;
+  height: 38px;
+  background-color: #ff5454;
+  align-items: center;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  font-weight: bolder;
+  font-size: 18px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+}
+
+.login-f {
+  background-color: #161722;
+  border: 2px solid #fff;
+}
+
+.userimg {
+  width: 32px;
+  height: 32px;
+  background-color: red;
+  margin-right: 30px;
+  border-radius: 16px;
+}
+
+.seachmove {
+  color: #fe2c55;
+}
+</style>
---
+++ love-pet-front/src/views/Search/Search.vue
@@ -0,0 +1,280 @@
+<template>
+  <div id="Search">
+    <div class="main">
+      <HeaderLogo></HeaderLogo>
+      <div class="content">
+        <div class="content_top" v-for="item in search_count" :key="item.id">
+          <!-- 头像 -->
+          <div class="header_photo">
+            <img alt="" :src="item.url" />
+          </div>
+
+          <div class="header_work">
+            <!-- 名字 -->
+            <div class="word">
+              {{ item.name }}
+            </div>
+            <!-- 获赞和粉丝 -->
+            <div class="word_bottom">
+              {{ item.zang }}获赞
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {{ item.feng }}粉丝
+            </div>
+          </div>
+
+          <!-- 关注 -->
+          <div class="attention">
+            <button>关注</button>
+          </div>
+        </div>
+
+        <!-- 搜索数据 -->
+        <div class="search_data">
+          <div class="search_box" v-for="item in search_list" :key="item.id">
+            <!-- 头像 -->
+            <div class="search_photo">
+              <img alt="" :src="item.photo_utl" />
+            </div>
+            <!-- 名字时间 -->
+            <div class="search_main">
+              <!-- 名字 -->
+              <div class="search_name">{{ item.name }}</div>
+              <!-- 时间 -->
+              <div class="search_time">{{ item.time }}</div>
+
+              <!-- 内容 -->
+              <div class="search_content">
+                <div class="search_content_title">{{ item.content }}</div>
+                <div class="search_content_main" v-if="item.type == 'photo'">
+                  <img alt="" :src="item.content_photo" />
+                </div>
+                <div class="search_content_main" v-if="item.type == 'video'">
+                  <video  controls="controls" :src="item.content_video">
+                  </video>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderLogo from "@/components/HeaderLogo/HeaderLogo.vue";
+export default {
+  data() {
+    return {
+      search_count: [
+        {
+          id: 1,
+          name: "xxx",
+          zang: "666w",
+          feng: "666w",
+          url: "http://localhost:8080/img/cat.db8703c1.png",
+        },
+      ],
+      search_list: [
+        {
+          id: 1,
+          name: "xxx",
+          photo_utl: "http://localhost:8080/img/cat.db8703c1.png",
+          time: "2022-1-23",
+          content: "xxx好可爱!",
+          // text 是只有文字 photo是有文字和图片 video是文字和视频
+          type: "video",
+          content_photo: "",
+          content_video: "https://www.w3school.com.cn/i/movie.ogg",
+        },
+        {
+          id: 1,
+          name: "xxx",
+          photo_utl: "http://localhost:8080/img/cat.db8703c1.png",
+          time: "2022-1-23",
+          content: "xxx好可爱!",
+          // text 是只有文字 photo是有文字和图片 video是文字和视频
+          type: "photo",
+          content_photo: "http://localhost:8080/img/userimg.aae57e08.png",
+          content_video: "",
+        },
+      ],
+    };
+  },
+  components: {
+    HeaderLogo,
+  },
+};
+</script>
+
+<style>
+@import "//at.alicdn.com/t/font_2876277_7ex4php5szg.css";
+
+#Search {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+.main {
+  flex-grow: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: calc(100% - 60px);
+  flex: 1 1 auto;
+  background-color: #161722;
+  position: relative;
+  box-sizing: border-box;
+  flex-shrink: 0;
+  overflow: hidden;
+}
+
+.content_top {
+  width: 857px;
+  height: 117px;
+  background-color: #2e2f3b;
+  border-radius: 4px;
+  margin: 61px auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.header_photo {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #fff;
+  display: flex;
+  margin-left: 42px;
+}
+
+.header_photo img {
+  width: 60px;
+  border-radius: 50%;
+}
+
+.word {
+  color: #fff;
+  font-size: 16px;
+  display: flex;
+  margin-left: 20px;
+}
+
+.word_bottom {
+  margin-left: 20px;
+  margin-top: 25px;
+  display: flex;
+  color: #fff;
+  font-size: 14px;
+  align-items: flex-end;
+}
+
+.header_work {
+  display: flex;
+  flex-direction: column;
+}
+
+.attention {
+  display: flex;
+  flex-direction: row-reverse;
+  flex-grow: 1;
+  margin-right: 20px;
+}
+
+.attention button {
+  background-color: #fe2c55;
+  width: 65px;
+  height: 30px;
+  border-radius: 4px;
+  border: 0;
+  color: #fff;
+}
+
+.attention button:hover {
+  background-color: #f22a51;
+}
+
+.search_data {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.search_box {
+  width: 857px;
+  background-color: rgba(22, 23, 34, 1);
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+  border: 1px solid rgba(128, 128, 128, 1);
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+
+}
+
+.search_photo {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  margin-left: 20px;
+  margin-top: 20px;
+}
+
+.search_photo img {
+  width: 50px;
+  border-radius: 50%;
+}
+
+.search_main {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 10px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.search_name {
+  color: #fff;
+  font-size: 16px;
+}
+
+.search_time {
+  margin-top: 10px;
+  color: #828285;
+  font-size: 12px;
+}
+
+.search_content_main {
+  margin-top: 20px;
+}
+.search_content_main img {
+  width: 73px;
+  height: 80px;
+  border-radius: 4px;
+}
+
+.search_content_title {
+  color: #fff;
+  font-size: 16px;
+}
+
+.search_content {
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+}
+</style>
